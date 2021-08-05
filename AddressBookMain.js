68a201339b5a8b74726379fea7b6e61ddc6e800b
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-05 21:00:03 +0530

    Added calling method for adding new contacts

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -1,1 +1,24 @@
-console.log("Address book problem");
+import * as addressBook from "./AddressBook.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const prompt = require('prompt-sync')({ sigint: true });
+
+let choice = 0;
+do {
+    console.log("\n1. Add Contact\n2. Display contacts\n3. Exit");
+    choice = Number(prompt("Enter your choice number: "));
+    switch (choice) {
+        case 1:
+            addressBook.addContact();
+            break;
+        case 2:
+            addressBook.displayContacts();
+            break;
+        case 3:
+            console.log("You have now quit the program.");
+            break;
+        default:
+            console.log("Invalid Choice!");
+            break;
+    }
+} while (choice != 3);
